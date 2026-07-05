bbe76ade873c7cd8966a343a3f83045b8757f566
Arda Öztürk <66124224+05Arda@users.noreply.github.com>
2026-07-05 22:04:11 +0300

    fix directory error

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
 from datetime import datetime, UTC
 
 MODEL_ID = "unsloth/gemma-4-E2B-it"
-OUTPUT_DIR = "finetuned_models" / f"./model_{datetime.now(UTC).timestamp()}"
+OUTPUT_DIR = f"./finetuned_models/model_{datetime.now(UTC).timestamp()}/"
